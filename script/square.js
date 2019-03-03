db04e824c5e07b77bdf704da1507f76e34a2db29
BharathKanagala <bharathkanagala19@gmail.com>
2019-03-03 14:12:29 -0600

    local storage

--- script/square.js
+++ script/square.js
@@ -1,5 +1,6 @@
 document.getElementById('submit').addEventListener('click', square);
 document.getElementById('submit').addEventListener('click', joke);
+document.getElementById('getlocal').addEventListener('click', get);
 
 function joke() {
     $.ajax({
@@ -22,5 +23,10 @@ let value=document.getElementById('numberOne').value;
     value = parseInt(value);
     console.log(value * value);
     document.getElementById('numberTwo').innerHTML = value * value;
+    localStorage.setItem("localvalues",value*value);
     return value * value;
+}
+function get(){
+document.getElementById('lc').value=localStorage.getItem("localvalues");
+
 }
